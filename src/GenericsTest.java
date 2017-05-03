2fb9647f2e7d3332f8e4cbb40aee8677c07a7abc
arkan382382 <arkan382382@outlook.com>
2017-05-03 20:12:06 +0200

    Initial commit

---
+++ src/GenericsTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created by arkan on 03.05.2017.
+ *
+ * https://docs.oracle.com/javase/tutorial/java/generics/index.html
+ */
+public class GenericsTest {
+    public static void main(String[] args){
+
+    }
+}
