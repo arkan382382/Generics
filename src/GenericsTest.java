55ff7594d23f8fa7282bed0a0957e6106ec28d71
Sebastian <arkan382382@outlook.com>
2017-05-04 19:53:56 +0200

    first steps with generics

--- src/GenericsTest.java
+++ src/GenericsTest.java
@@ -6,5 +6,29 @@
 public class GenericsTest {
     public static void main(String[] args){
 
+        JavaMagicBox<Integer> jBox1 = new JavaMagicBox<Integer>();
+        JavaMagicBox<String> jBox2 = new JavaMagicBox<String>();
+
+        jBox1.box = 57;
+
+        jBox2.box = "ada";
+        jBox2.box2 = "papa";
+        System.out.printf(jBox2.JoinString(jBox2.box, jBox2.box2));
+
+    }
+}
+
+class JavaMagicBox<T> {
+    public T box;
+    public T box2;
+
+
+    public String JoinString(T a, T b){
+        return a.toString() + b.toString();
+    }
+
+    public <T> void print(T[] array){
+        for(T element : array)
+            System.out.println(element + " ");
     }
 }
